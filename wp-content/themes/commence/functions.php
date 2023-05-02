3054d5ae602ab2df4e85c1f84903e836ad804f05
Jose Rios <jose@commence.studio>
2023-05-02 16:59:54 -0700

    :rocket: Add shortcode for copyright

--- wp-content/themes/commence/functions.php
+++ wp-content/themes/commence/functions.php
@@ -7,3 +7,11 @@ function my_theme_enqueue_styles() {
     wp_enqueue_style( 'child-style', get_stylesheet_uri() );
 }
 
+// For Copyright
+
+function year_shortcode () {
+    $year = date_i18n ('Y');
+    return $year;
+}
+
+add_shortcode ('year', 'year_shortcode');
